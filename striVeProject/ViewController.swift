7e9dbd716fbf54755e53e32bda6f6fabec7376a4
Sneha Bhamidipaty <snehabhamidipaty@gmail.com>
2022-06-30 16:33:54 -0500

    finalIteration

--- striVeProject/ViewController.swift
+++ striVeProject/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blue: UILabel!
+    @IBOutlet weak var red: UILabel!
+    
+    @IBOutlet weak var v: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func invisible(_ sender: Any) {
+        UIView.animate(withDuration : 1, animations : {self.blue.frame.origin.x -= 105}, completion: nil)
+        UIView.animate(withDuration : 1, animations : {self.red.frame.origin.x += 105}, completion: nil)
+        /*UIView.animate(withDuration : 1, animations : {self.v.frame.size = (width: 10.0, height: 10.0)}, completion: nil)
+         */
+    }
+    
+    
 }
 
